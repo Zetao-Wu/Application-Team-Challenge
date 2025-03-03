fe5b76eeffa92d03f363d648584f23b983918531
Zetao-Wu <zetao_wu@brown.edu>
2025-03-02 22:53:27 -0500

    api

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import "globals.css"
 import Navbar from "./components/Navbar";
 import ParticipantsTable from "./components/ParticipantsTable";
+
 
 export default function Home() {
   return (
