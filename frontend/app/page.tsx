ed6ee62e691b003e7edbfdc02143af98cb5ae0ef
Zetao-Wu <zetao_wu@brown.edu>
2025-03-02 22:57:59 -0500

    ds

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import "globals.css"
+import "globals.css";
 import Navbar from "./components/Navbar";
 import ParticipantsTable from "./components/ParticipantsTable";
 
